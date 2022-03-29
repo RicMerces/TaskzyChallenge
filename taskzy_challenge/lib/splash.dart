866f7894cb077f1150e79d0b352806f6ca23d31c
RicMerces <ricardoliveira0703@gmail.com>
2022-03-29 18:23:01 -0300

    Color

---
+++ taskzy_challenge/lib/splash.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff306fd5),
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
