654e1709c18f3beaad37a3058c0b2a3559ca6c7a
RicMerces <ricardoliveira0703@gmail.com>
2022-03-30 15:24:33 -0300

    Upgrade Flutter Version

---
+++ taskzy/android/app/src/main/kotlin/com/example/taskzy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.taskzy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- taskzy_challenge/android/app/src/main/kotlin/com/example/taskzy_challenge/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.taskzy_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
