75f993243536d659301accbac1d9801852fa88a8
RicMerces <ricardoliveira0703@gmail.com>
2022-03-31 16:14:30 -0300

    Screen

--- taskzy/lib/main.dart
+++ taskzy/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:taskzy/pages/onBoarding01.dart';
+import 'package:taskzy/pages/onBoarding.dart';
 import 'pages/splash.dart';
 
 void main() {
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Onboarding01(),
+      home: Onboarding(),
     );
   }
 }
---
+++ taskzy/lib/pages/onBoarding.dart
@@ -0,0 +1,54 @@
+import 'package:taskzy/main.dart';
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF2EFE9),
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 80),
+        margin: const EdgeInsets.only(left: 50, right: 50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/Pin.png'),
+            RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                text: 'Um jeito',
+                style: TextStyle(
+                  color: Color(0xff306FD5),
+                  fontSize: 36,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' novo',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(text: ' para organizar suas'),
+                  TextSpan(
+                    text: ' tasks',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomSheet: Container(
+        padding: const EdgeInsets.symmetric(),
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+      ),
+    );
+  }
+}
