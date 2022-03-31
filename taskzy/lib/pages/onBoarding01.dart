c6961c7e7022b4dafe3b12ef6f3e5c229ff8508f
RicMerces <ricardoliveira0703@gmail.com>
2022-03-31 08:17:29 -0300

    Remove windows runner

--- taskzy/lib/pages/onBoarding01.dart
+++ taskzy/lib/pages/onBoarding01.dart
@@ -9,6 +9,21 @@ class Onboarding01 extends StatefulWidget {
 class _Onboarding01State extends State<Onboarding01> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      backgroundColor: Color(0xffF2EFE9),
+      body: Container(
+        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+        child: Column(
+          children: [
+            // RichText(text: Text('data'))
+            Image.asset('assets/images/Pin.png'),
+            // const Text(
+
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
   }
 }
