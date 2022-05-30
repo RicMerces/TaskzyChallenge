c5a485eba0a12a6b9326f16cceea730d81bf6369
RicMerces <ricardoliveira0703@gmail.com>
2022-05-30 17:18:25 -0300

    Registro

--- taskzy/lib/pages/Registro.dart
+++ taskzy/lib/pages/Registro.dart
@@ -46,31 +46,141 @@ class _RegistroState extends State<Registro> {
                 ),
                 Column(
                   children: [
-                    TextField(
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(labelText: 'Nome'),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: 'Nome',
+                            contentPadding: EdgeInsets.all(18),
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 0, color: Color(0xff306FD5)),
+                                borderRadius: BorderRadius.circular(5))),
+                      ),
                     ),
-                    TextField(
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(labelText: 'Usuario'),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: 'Usuario',
+                            contentPadding: EdgeInsets.all(18),
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 3, color: Color(0xff306FD5)),
+                                borderRadius: BorderRadius.circular(5))),
+                      ),
                     ),
-                    TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(labelText: 'Email'),
+                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 14),
+                        child: Text(
+                          'Seja criativo!',
+                        ),
+                      ),
+                    ]),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: 'E-mail',
+                            contentPadding: EdgeInsets.all(18),
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 3, color: Color(0xff306FD5)),
+                                borderRadius: BorderRadius.circular(5))),
+                      ),
                     ),
-                    TextField(
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: true,
-                      decoration: InputDecoration(labelText: 'Senha'),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: 'Senha',
+                            contentPadding: EdgeInsets.all(18),
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 3, color: Color(0xff306FD5)),
+                                borderRadius: BorderRadius.circular(5))),
+                      ),
                     ),
-                    TextField(
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: true,
-                      decoration:
-                          InputDecoration(labelText: 'Confirmação de Senha'),
+                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 14),
+                        child: Text(
+                          '- 8 ou mais caracteres',
+                        ),
+                      ),
+                    ]),
+                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 14),
+                        child: Text(
+                          '- Letras e números',
+                        ),
+                      ),
+                    ]),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: TextField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            labelText: 'Confirmação de Senha',
+                            contentPadding: EdgeInsets.all(18),
+                            border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    width: 3, color: Color(0xff306FD5)),
+                                borderRadius: BorderRadius.circular(5))),
+                      ),
                     ),
+                    Column(children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Já tem uma conta?',
+                              style: TextStyle(
+                                  color: Color(0xff4D4D4D), fontSize: 15),
+                            ),
+                            TextButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  'Faça login.',
+                                  style: TextStyle(
+                                    color: Color(0xff306FD5),
+                                    fontSize: 15,
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 40),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Registro',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
+                          ),
+                          style: TextButton.styleFrom(
+                            backgroundColor: Color(0xff306FD5),
+                            fixedSize: Size(318, 67),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                        ),
+                      )
+                    ])
                   ],
-                )
+                ),
               ],
             ),
           ),
